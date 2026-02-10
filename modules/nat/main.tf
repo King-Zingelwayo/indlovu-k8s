b4cffb3612ed4ecb4b9e8d20fa66e15b9119cd47
Sihle Ndlovu <sihle@synthesis.co.za>
2026-02-10 12:07:10 +0200

    fixed nAT

--- modules/nat/main.tf
+++ modules/nat/main.tf
@@ -4,7 +4,7 @@ data "aws_ami" "nat" {
 
   filter {
     name   = "name"
-    values = ["amzn-ami-vpc-nat-*"]
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
   }
 
   filter {
@@ -20,6 +20,14 @@ resource "aws_instance" "nat" {
   vpc_security_group_ids = [var.nat_sg_id]
   source_dest_check      = false
   iam_instance_profile   = var.ssm_profile_name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo 1 > /proc/sys/net/ipv4/ip_forward
+              iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+              yum install -y iptables-services
+              service iptables save
+              EOF
 
   tags = merge(var.tags, {
     Name = "${var.cluster_name}-nat"
